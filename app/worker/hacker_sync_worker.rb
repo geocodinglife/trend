9c51517e1ac2aa4eabf15dd172ebd3c74a98a586
ricardo alarcon <ricardo.alarcon@mobomo.com>
2021-02-27 16:54:25 -0500

    debuging

--- app/worker/hacker_sync_worker.rb
+++ app/worker/hacker_sync_worker.rb
@@ -1,5 +1,6 @@
 class HackerSyncWorker
-  options = Selenium::WebDriver::Chrome::Options.new.add_argument('--headless')
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
   driver = Selenium::WebDriver.for :chrome, options: options
 
   number = 0
